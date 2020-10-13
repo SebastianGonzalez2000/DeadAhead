4373a94e2d5ee9efab6bb17ac3f8173ddb7a8853
sebastiangonzalez <bastigonzalez2000@gmail.com>
2020-10-13 09:29:14 -0700

    Improved code coverage for the switch statement.

--- src/main/model/Player.java
+++ src/main/model/Player.java
@@ -32,7 +32,7 @@ public class Player {
             case WEST:
                 xc += -speed;
                 return;
-            case EAST:
+            default:
                 xc += speed;
         }
     }
